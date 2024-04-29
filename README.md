8c8c725f7cc924b06e2671c9f60963f0c1a7bebe
Ivan Arevalo <idarevalog@ltutech.com>
2024-04-29 18:12:40 +0200

    Added first sample

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # torchSamples
+
 Simple code in C++ and python to test torch and pytorch functions
+
+## Run docker shell
+First set env:
+```
+. .env
+```
+
+then build the image:
+```
+build_docker_img
+```
+
+then run shell:
+```
+run_docker_shell
+```
+
+## Build sample
+1. First run a docker shell
+2. `cd` to the sample you want to build, for example:
+```
+cd /app/samples/print_tensors
+```
+3. cmake build:
+```
+mkdir build
+cd build
+cmake ..
+cmake --build .
+```
--- samples/print_tensors/main.cpp
+++ samples/print_tensors/main.cpp
@@ -3,10 +3,15 @@
 
 int main() {
     // Create a tensor of size (2, 3) filled with random values
-    auto tensor = torch::randn({2, 3});
-    
-    // Print the tensor
-    std::cout << "Tensor:\n" << tensor << std::endl;
-    
+    auto tensor1 = torch::randn({2, 3});
+
+    // // Print the tensor
+    std::cout << "Tensor 1:\n" << tensor1 << std::endl;
+
+    // User values tensor
+    auto tensor2 = torch::tensor({{1, 2}, {3, 4}});
+    // // Print the tensor
+    std::cout << "Tensor 2:\n" << tensor2 << std::endl;
+
     return 0;
 }
