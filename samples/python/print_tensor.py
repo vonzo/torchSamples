ddec2d82515e911be4540a6c790bfadff0d7b1ce
Ivan Arevalo <idarevalog@ltutech.com>
2024-04-30 17:48:00 +0200

    Add pytorch

---
+++ samples/python/print_tensor.py
@@ -0,0 +1,15 @@
+import torch
+
+# Create a random tensor
+tensor1 = torch.randn(2, 3)
+
+# Print the tensor
+print("Tensor 1:")
+print(tensor1)
+
+# Create a tensor with user-defined values
+tensor2 = torch.tensor([[1, 2], [3, 4]])
+
+# Print the tensor
+print("Tensor 2:")
+print(tensor2)
